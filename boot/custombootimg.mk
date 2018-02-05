b58b7bd6a572e8c55ddf7952b13ffbcffe41fe19
Caio Oliveira <caiooliveirafarias0@gmail.com>
2018-02-05 21:57:10 -0200

    boot: custombootimg: Add building boot step
    Change-Id: I04fa470e90b65ae88d44219410d45a89abd6c5d1
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -15,3 +15,16 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): \
 		--output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	$(call pretty,"Made recovery image: $@")
+
+$(INSTALLED_BOOTIMAGE_TARGET): \
+		$(MKBOOTIMG) \
+		$(INTERNAL_BOOTIMAGE_FILES) \
+		$(INSTALLED_DTIMAGE_TARGET)
+	@echo -e ${CL_CYN}"----- Making boot image ------"${CL_RST}
+	$(hide) $(MKBOOTIMG) \
+		$(INTERNAL_BOOTIMAGE_ARGS) \
+		$(BOARD_MKBOOTIMG_ARGS) \
+		--dt $(PRODUCT_OUT)/dt.img \
+		--output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+	$(call pretty,"Made boot image: $@")
