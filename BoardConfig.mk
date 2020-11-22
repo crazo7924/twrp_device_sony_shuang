39915d69d540f2f3d0bc5d03adbd821637ab9ca0
Bharat Dev Burman <bharat.singh7924@gmail.com>
2020-11-22 13:19:57 +0530

    shuang: cpu variant is Cortex A7
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
+TARGET_CPU_VARIANT := cortex-a7
 
 # Kernel properties
 TARGET_KERNEL_SOURCE := kernel/sony/msm8x10
